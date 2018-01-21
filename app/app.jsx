ffb84450657b3b9ffa7f28fb50191ee3900d3655
jtackettFS <32463694+jtackettFS@users.noreply.github.com>
2018-01-21 10:36:04 -0500

    Initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 // Local requires
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -13,6 +14,6 @@ require('style!css!sass!applicationStyles')
 $(document).foundation();
 
 ReactDOM.render(
-    <p>Boilerplate 3 Project</p>,
+    <TodoApp/>,
     document.getElementById('app')
 );
