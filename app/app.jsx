fe7bcfa679a17c009ee72b7dc6a23536f5679a8a
jtackettFS <jtackett@fullsail.com>
2018-03-21 11:39:03 -0400

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 // Local requires
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+	console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Go to Costco'));
+store.dispatch(actions.setSearchText('Costco'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
